0157584776809a3a59305b9fd03b1f8734931877
Mohit Bansal <mohit1291@chitkara.edu.in>
2024-07-20 18:53:28 +0530

    fix: add unimplemented error code for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,37 @@
+const {StatusCodes}=require('http-status-codes');
+
 function pingProblemController(req,res){
     res.json({message:"Problem controller is up"})
 }
 
 function addProblem(req,res){
-
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 }
 
 function getProblem(req,res){
-
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 }
 
 function getProblems(req,res){
-
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 }
 
 function deleteProblem(req,res){
-    
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 }
 
 function updateProblem(req,res){
-
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+    })
 }
 
 module.exports={
