d0f8a0edfb2d5c534a9c77f0b251e68cc6a401d8
Mohit Bansal <mohit1291@chitkara.edu.in>
2024-08-12 11:32:11 +0530

    feat:Completed create problem api

---
+++ src/services/problem.service.js
@@ -0,0 +1,23 @@
+const sanitizeMarkdownContent = require("../utils/markdownSanitizer");
+
+class ProblemService {
+    constructor(ProblemRepository){
+        this.ProblemRepository = ProblemRepository;
+    }
+
+    async createProblem (problemData) {
+        try {
+            // Sanitize markdown for the description
+            problemData.description = sanitizeMarkdownContent(problemData.description);
+
+            const problem = await this.ProblemRepository.createProblem(problemData);
+
+            return problem;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+}
+
+module.exports = ProblemService;
