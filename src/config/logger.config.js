fd9720a1fa0f2fc2248a29ab87c4ec5a371d3261
Mohit Bansal <mohit1291@chitkara.edu.in>
2024-12-27 13:46:38 +0530

    Revert "feat:added cosmos db for logs"
    This reverts commit 58d4b630263520d20f2e12e77ebcf5495b8b1948.

--- src/config/logger.config.js
+++ src/config/logger.config.js
@@ -1,25 +1,7 @@
 const winston = require('winston');
 const { LOG_DB_URL } = require('./server.config');
-require('winston-mongodb');
-const {Writable} = require('stream');
-const {logToCosmosDB} = require('../clientapis/cosmosClient');
-
 const allowedTransports = [];
-
-const customTransport = new Writable({
-    write(chunk, encoding, callback) {
-        const message = chunk.toString();
-        console.log("Log intercepted in custom transport ",message);
-        logToCosmosDB("error",message);
-        callback();
-    }
-})
-
-const customStreamTransport = new winston.transports.Stream({
-    stream:customTransport
-});
-
-allowedTransports.push(customStreamTransport);
+require('winston-mongodb');
 
 // The Below Transport configuration enables logging on the console
 allowedTransports.push(new winston.transports.Console({
